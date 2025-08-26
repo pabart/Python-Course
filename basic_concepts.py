06ce27fed6e59613ebdec19a87a4207848a1c5f0
Pablo Aleman <pabart.aleman@gmail.com>
2025-08-25 19:59:29 -0600

    Adds basic data types file

---
+++ basic_concepts.py
@@ -0,0 +1,27 @@
+# Comments
+# This is a single-line comment
+"""
+This is a 
+multi-line comment
+"""
+
+#Integers
+#Integers are numbers that do not have a decimal part. For example:
+age = 29
+quantity = 100
+print (age)
+
+#Float
+#Floating point-numbers are those that have a decimal part.
+price = 9.99
+height = 1.65
+
+#Strings
+#Strings are sequences of characters enclosed in single quotes ('...') or double quotes ("...")
+name = "Pablo"
+message = "Hello, everyone"
+
+#Booleans
+#Booleans represent truth values: True and False.
+is_adult =True
+has_discount = True
