f9e8fc62b989aea1711097da41e903fc0831bc22
Pablo Aleman <pabart.aleman@gmail.com>
2025-08-25 20:11:27 -0600

    Adds operators file

---
+++ operators.py
@@ -0,0 +1,38 @@
+# Operators
+# Operartors are special symbols that allow us to perform operations on variables and values.
+
+# Arithmetic
+# Are used to perform basic mathematical operations
+
+a = 10
+b = 3
+
+sum = a + b # 13
+substraction = a - b # 7
+multiplication = a * b # 30
+division = a / b # 3.3333333
+floor_division = a//b # 3
+modulus = a % b # 1
+exponentation = a ** b # 1000
+
+#Comparison
+# Are used to compare two values and return a boolean value
+
+a = 10
+b = 3
+equal = a == b # False
+not_equal = a != b # True
+greater_than = a > b # True
+less_than = a < b # False
+greater_or_equal = a >= b # True
+less_or_equal = a <= b # False
+
+# Logical
+# Are used to combina conditional expressions and evaluate multiple conditions
+
+a = 10
+b = 3
+
+result_and = (a > 5) and (b < 5) # True
+result_or = (a > 15) or (b < 5) # True
+result_not = not (a > 5) # False
