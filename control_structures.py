8d7dbc10c63cf25be85fd5faeb9fb6af8c0b4c0a
Pablo Aleman <pabart.aleman@gmail.com>
2025-08-25 20:21:36 -0600

    Adds file for control structures and if statement

---
+++ control_structures.py
@@ -0,0 +1,19 @@
+# Control structures
+# Allows us to control the flow of execution of our programs
+
+# Conditional Structures
+# Allows us to execute different blocks of code depending on whether or not a certain condition is met.
+
+age = 18
+# If
+if age <= 3 :
+    print ("You are a baby")
+elif age <= 13:
+    print ("You are a kid")
+elif age <= 19:
+    print ("You are a teenager")
+else:
+    print ("You are an adult")
+
+
+# Loops
