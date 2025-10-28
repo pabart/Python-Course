d3100342a95a369699be3863201c5c48f166b4bb
Pablo Aleman <pabart.aleman@gmail.com>
2025-10-27 19:11:19 -0600

    Adds sets method and brief explanation for what data structures in python are.

--- data structures/sets.py
+++ data structures/sets.py
@@ -27,3 +27,37 @@ print("The difference between set1 and set2 is: ", difference)
 
 symmetric_difference = set1 ^ set2
 print("The symmetric difference is:", symmetric_difference)
+
+# Set methods
+# Sets in methods have several built-in methods to manipulate and access elements. Some common methods are:
+
+# add(element): adds an element to the set
+# remove(element): removes an element from the set, if the element does not exists it raises an error.
+# discard(element): removes an element from the set if present. If the element does not exists, it does nothing.
+# clear(): removes all elements from the set.
+
+# Example:
+
+fruits = {"apple", "banana", "orange"}
+
+print("This is the set before doing an changes: ", fruits)
+
+print("We add one element to the set 'pear':")
+fruits.add("pear")
+print(fruits)
+
+print("We remove one element from the set 'apple'")
+fruits.remove("apple")
+print(fruits)
+
+print("We discard one element from the set 'grape'")
+fruits.discard("grape")
+print(fruits)
+
+print("We remove all elements from the set")
+fruits.clear()
+print(fruits)
+
+# Data structures provide us with great flexibility and power to store and manipulate data in our programs. Lists are
+# useful for ordered and mutable collections, tuples are ordered and immutable collections, dictionaries for storing
+# key-value pairs and sets for unordered collections and unique elements.
